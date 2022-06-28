4f683799f209345fbafc14bb916670e77b5d1277
Jay Lee <jaylee.possible@gmail.com>
2022-06-29 03:23:52 +0900

    feat: add white board

--- packages/post-it/src/App.tsx
+++ packages/post-it/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Wysiwyg from '@code-logs/wysiwyg'
+import './styles/global.scss'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Wysiwyg />
-      </header>
+      <header className="App-header"></header>
+      <main>
+        <div id="wysiwyg-container">
+          <Wysiwyg />
+        </div>
+      </main>
     </div>
   )
 }
