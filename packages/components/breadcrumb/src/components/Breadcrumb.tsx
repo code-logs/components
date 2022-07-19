8d53f37c6646d4035a39b1353a7cf4395e9c1cb6
Jay Lee <jaylee.possible@gmail.com>
2022-07-19 11:51:49 +0900

    feat: add breadcrumb ui component

---
+++ packages/components/breadcrumb/src/components/Breadcrumb.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import { Path } from '../types'
+import '../styles/breadcrumb.scss'
+
+export interface BreadcrumbProps {
+  paths: Path[]
+  delimiter?: React.ReactElement
+  onRoute?: (route: Path['route']) => void
+}
+
+const Breadcrumb = ({
+  paths,
+  delimiter = <span>{' > '}</span>,
+  onRoute,
+}: BreadcrumbProps) => {
+  const renderBreadcrumb = (
+    { name, route }: Path,
+    appendDelimiter: boolean
+  ) => {
+    if (route) {
+      return (
+        <>
+          <a
+            className="list-item"
+            href={route}
+            onClick={(event) => {
+              if (!onRoute) return
+              event.preventDefault()
+              onRoute(route)
+            }}
+          >
+            {name}
+          </a>
+          {appendDelimiter ? delimiter : <></>}
+        </>
+      )
+    } else {
+      return (
+        <>
+          <span className="list-item">{name}</span>
+          {appendDelimiter ? delimiter : <></>}
+        </>
+      )
+    }
+  }
+
+  if (!paths.length) return <></>
+
+  return (
+    <section className="breadcrumb">
+      <ol className="list">
+        {paths.map((path, index) => (
+          <li key={path.name} className="name">
+            {renderBreadcrumb(path, index < paths.length - 1)}
+          </li>
+        ))}
+      </ol>
+    </section>
+  )
+}
+
+export default Breadcrumb
